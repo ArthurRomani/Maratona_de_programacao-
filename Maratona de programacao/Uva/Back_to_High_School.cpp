0e6c252ccba329451a54873d8a2c6a98822ba374
Arthur Romani Inacio de Souza <125612826+ArthurRomani@users.noreply.github.com>
2025-06-04 00:42:45 -0300

    Add files via upload

---
+++ Maratona de programacao/Uva/Back_to_High_School.cpp
@@ -0,0 +1,21 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int v, t;
+    while (cin >> v >> t)
+    {
+        cout << 2 * v * t << endl;
+    }
+
+    return 0;
+}
